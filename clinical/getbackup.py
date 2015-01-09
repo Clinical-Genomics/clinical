135eedd9cd216c2456eef090dae8d08e8969b1ef
Rikard <rikard.erlandsson@scilifelab.se>
2015-01-09 15:49:46 +0100

    Update getbackup.py

--- clinical/getbackup.py
+++ clinical/getbackup.py
@@ -14,47 +14,9 @@ import subprocess
 import psutil
 
 # ssh -fN -L 3307:localhost:3306 hiseq.clinical@clinical-db.scilifelab.se
-cmnd = ['ssh', '-f', '-N', '-L', '3307:localhost:3306', 'hiseq.clinical@clinical-db.scilifelab.se']
+cmnd = 'ssh -fN -L 3307:localhost:3306 hiseq.clinical@clinical-db.scilifelab.se'
 
-
-# ["ssh", "%s" % HOST, COMMAND]
-
-ssh_process = subprocess.Popen(cmnd,
-                       shell=False
- #                      stdin=subprocess.PIPE,
- #                      stdout=subprocess.PIPE,
- #                      stderr=subprocess.PIPE
-                       )
-#result = ssh.stdout.readlines()
-#if result == []:
-#    error = ssh.stderr.readlines()
-#    print >>sys.stderr, "ERROR: %s" % error
-#else:
-#    print result
-print str(ssh_process.pid)
-
-while True:
-  p = ssh_process.poll()
-  if p is not None: break
-  time.sleep(1)
-
-if p == 0:
-        # Unfortunately there is no direct way to get the pid of the spawned ssh process, so we'll find it
-        # by finding a matching process using psutil.    proc.cmdline == tunnel_cmd.split() and 
- 
-  current_username = psutil.Process(os.getpid()).username
-  ssh_processes = [proc for proc in psutil.get_process_list() if proc.username == current_username]
-  for pc in psutil.get_process_list():
-    print str(pc.cmdline)
- 
-  if len(ssh_processes) == 1:
-    print ssh_processes[0]
-  else:
-    raise RuntimeError, 'multiple (or zero?) tunnel ssh processes found: ' + str(ssh_processes) 
-else:
-  raise RuntimeError, 'Error creating tunnel: ' + str(p) + ' :: ' + str(ssh_process.stdout.readlines())
-
-ssh_process_id = subprocess.Popen("ps aux | grep 'ssh -fN -L 3307:localhost:3306 hiseq.clinical@clinical-db.scilifelab.se' | grep -v grep | awk '{print $2}'", shell=False)
+tunnel_pid = create_tunnel(cmnd)
 
 pars = readconfig('hej')
 print pars['CLINICALDBUSER']
@@ -82,7 +44,7 @@ else:
 dbclose(cnx, cursor)
 
 p = ssh_process.poll()
-print str(p), ssh_process_id
-ssh_process_id.terminate()
+print str(p), tunnel_pid
+tunnel_pid.terminate()
 
 exit(0)
