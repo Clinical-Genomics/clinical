41d296515cc416ed15537702ed8a49604886ed8b
Rikard <rikard.erlandsson@scilifelab.se>
2015-01-09 12:31:40 +0100

    Update getbackup.py

--- getbackup.py
+++ getbackup.py
@@ -22,6 +22,7 @@ cmnd = ['ssh', '-f', '-N', '-L', '3307:localhost:3306', 'hiseq.clinical@clinical
 
 ssh = subprocess.Popen(cmnd,
                        shell=False,
+                       stdin=subprocess.PIPE,
                        stdout=subprocess.PIPE,
                        stderr=subprocess.PIPE
                        )
