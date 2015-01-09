9ceb56b5dc6dbaa8faedded59f6e0500338eeb03
Rikard <rikard.erlandsson@scilifelab.se>
2015-01-09 10:42:26 +0100

    Update getbackup.py

--- getbackup.py
+++ getbackup.py
@@ -14,4 +14,23 @@ from dbaccess import *
 pars = readconfig('hej')
 print pars['CLINICALDBUSER']
 
+cnx, cursor = dbconnect(pars['CLINICALDBHOST'], pars['CLINICALDBPORT'], pars['STATSDB'], 
+                        pars['CLINICALDBUSER'], pars['CLINICALDBPASSWD'])
+
+cursor.execute(""" SELECT major, minor, patch FROM version ORDER BY time DESC LIMIT 1 """)
+row = cursor.fetchone()
+if row is not None:
+  major = row[0]
+  minor = row[1]
+  patch = row[2]
+else:
+  sys.exit("Incorrect DB, version not found.")
+if (str(major)+"."+str(minor)+"."+str(patch) == _VERSION_):
+  print params['STATSDB'] + " Correct database version "+str(_VERSION_)+"   DB "+params['STATSDB']
+else:
+  exit (params['STATSDB'] + "Incorrect DB version. This script is made for "+str(_VERSION_)+" not for "
+         +str(major)+"."+str(minor)+"."+str(patch))
+
+dbclose(cnx, cursor)
+
 exit(0)
