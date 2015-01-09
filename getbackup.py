ebb7b56913db2b041a66f4d5003895cfe5f83ca1
Rikard <rikard.erlandsson@scilifelab.se>
2015-01-09 11:03:46 +0100

    Update getbackup.py

--- getbackup.py
+++ getbackup.py
@@ -10,6 +10,23 @@ import socket
 import os
 import select
 from dbaccess import *
+import subprocess
+
+HOST="clinical-db.scilifelab.se"
+# Ports are handled in ~/.ssh/config since we use OpenSSH
+COMMAND="uname -a"
+ 
+ssh = subprocess.Popen(["ssh", "%s" % HOST, COMMAND],
+                       shell=False,
+                       stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE)
+result = ssh.stdout.readlines()
+if result == []:
+    error = ssh.stderr.readlines()
+    print >>sys.stderr, "ERROR: %s" % error
+else:
+    print result
+
 
 pars = readconfig('hej')
 print pars['CLINICALDBUSER']
