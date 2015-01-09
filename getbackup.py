e0948a0d9ac3b03c022dbfe8971083ace6f31e6f
Rikard <rikard.erlandsson@scilifelab.se>
2015-01-09 12:18:18 +0100

    Update getbackup.py

--- getbackup.py
+++ getbackup.py
@@ -21,9 +21,10 @@ cmnd = ['ssh', '-f', '-N', '-L', '3307:localhost:3306', 'hiseq.clinical@clinical
 # ["ssh", "%s" % HOST, COMMAND]
 
 ssh = subprocess.Popen(cmnd,
-                       shell=False,
-                       stdout=subprocess.PIPE,
-                       stderr=subprocess.PIPE)
+                       shell=False
+#                       stdout=subprocess.PIPE,
+#                       stderr=subprocess.PIPE
+                       )
 #result = ssh.stdout.readlines()
 #if result == []:
 #    error = ssh.stderr.readlines()
