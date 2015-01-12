91c7957954a2fae7b75d953c85dd8755623731ca
Rikard <rikard.erlandsson@scilifelab.se>
2015-01-12 10:20:28 +0100

    Update dbaccess.py

--- clinical/dbaccess.py
+++ clinical/dbaccess.py
@@ -23,6 +23,30 @@ def readconfig( config ):
         Params[pv[0]] = pv[1]
   return Params
 
+def create_tunnel(tunnel_cmd):
+    ssh_process = subprocess.Popen(tunnel_cmd,  universal_newlines=True,
+                                                shell=True,
+                                                stdout=subprocess.PIPE,
+                                                stderr=subprocess.STDOUT,
+                                                stdin=subprocess.PIPE)
+    # Assuming that the tunnel command has "-f" and "ExitOnForwardFailure=yes", then the 
+    # command will return immediately so we can check the return status with a poll().
+    while True:
+        p = ssh_process.poll()
+        if p is not None: break
+        time.sleep(1)
+     if p == 0:
+        # Unfortunately there is no direct way to get the pid of the spawned ssh process, so we'll find it
+        # by finding a matching process using psutil.
+         current_username = psutil.Process(os.getpid()).username()
+        ssh_processes = [proc for proc in psutil.get_process_list() if proc.cmdline() == tunnel_cmd.split() and proc.username() == current_username]
+        if len(ssh_processes) == 1:
+            return ssh_processes[0]
+        else:
+            raise RuntimeError, 'multiple (or zero?) tunnel ssh processes found: ' + str(ssh_processes) 
+    else:
+        raise RuntimeError, 'Error creating tunnel: ' + str(p) + ' :: ' + str(ssh_process.stdout.readlines())
+
 def dbconnect( Host, Port, Db, User, Passwd): 
   Cnx = mysql.connect(user=User, port=int(Port), host=Host, passwd=Passwd, db=Db)
   Cursor = Cnx.cursor()
@@ -33,35 +57,21 @@ def dbclose( Cnx, Cursor):
   Cnx.close()
   return
 
-def create_tunnel(tunnel_cmd):
-#    print psutil.Process(os.getpid()).username()
-    ssh_process = subprocess.Popen(tunnel_cmd,  universal_newlines=True,
-                                                shell=True,
-                                                stdout=subprocess.PIPE,
-                                                stderr=subprocess.STDOUT,
-                                                stdin=subprocess.PIPE)
- 
-    # Assuming that the tunnel command has "-f" and "ExitOnForwardFailure=yes", then the 
-    # command will return immediately so we can check the return status with a poll().
- 
-    while True:
-        p = ssh_process.poll()
-        if p is not None: break
-        time.sleep(1)
- 
-    if p == 0:
-        # Unfortunately there is no direct way to get the pid of the spawned ssh process, so we'll find it
-        # by finding a matching process using psutil.
- 
-        current_username = psutil.Process(os.getpid()).username()
-#        print tunnel_cmd
-#        for proc in psutil.get_process_list():
-#          print proc.cmdline()
-        ssh_processes = [proc for proc in psutil.get_process_list() if proc.cmdline() == tunnel_cmd.split() and proc.username() == current_username]
-#        print current_username
-        if len(ssh_processes) == 1:
-            return ssh_processes[0]
-        else:
-            raise RuntimeError, 'multiple (or zero?) tunnel ssh processes found: ' + str(ssh_processes) 
-    else:
-        raise RuntimeError, 'Error creating tunnel: ' + str(p) + ' :: ' + str(ssh_process.stdout.readlines())
+def generalquery( cursor, query ):
+  try:
+    cursor.execute(query)
+  except mysql.IntegrityError, e: 
+    print "Error %d: %s" % (e.args[0],e.args[1])
+    exit("DB error")
+# handle a specific error condition
+  except mysql.Error, e:
+    print "Error %d: %s" % (e.args[0],e.args[1])
+    exit("Syntax error")
+# handle a generic error condition
+  except mysql.Warning, e:
+    exit("MySQL warning")
+# handle warnings, if the cursor you're using raises them
+  respons = cursor.fetchall()
+  return respons 
+  
+  
