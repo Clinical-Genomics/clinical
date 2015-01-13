bcacbc24361d55b31263b01f6e67c1a25632d909
Rikard <rikard.erlandsson@scilifelab.se>
2015-01-13 13:54:57 +0100

    Create getpreproc.py

---
+++ clinical/getpreproc.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+#
+import sys
+import datetime
+import time
+import glob
+import re
+import socket
+import os
+import select
+from dbaccess import *
+import subprocess
+import psutil
+
+runfolder = sys.argv[1]
+
+if (len(sys.argv)>2):
+  configfile = sys.argv[2]
+else:
+  configfile = 'None'
+pars = readconfig(configfile)
+
+if not os.path.isdir(pars['RUNFOLDER'] + runfolder):
+  sys.exit("No directory " + pars['RUNFOLDER'] + runfolder)
+
+tunnel_pid = create_tunnel(pars['TUNNELCMD'])
+
+cnx, cursor = dbconnect(pars['CLINICALDBHOST'], pars['CLINICALDBPORT'], pars['STATSDB'], 
+                        pars['CLINICALDBUSER'], pars['CLINICALDBPASSWD'])
+
+ver = versioncheck(cursor, pars['STATSDB'], pars['DBVERSION'])
+
+if not ver == 'True':
+  print "Wrong db " + ver
+  dbclose(cnx, cursor)
+  tunnel_pid.terminate()
+  exit(0) 
+
+starttonas = str(datetime.datetime.fromtimestamp(os.path.getmtime(pars['RUNFOLDER'] + runfolder + "/RunInfo.xml" )))
+endtonas = str(datetime.datetime.fromtimestamp(os.path.getmtime(pars['RUNFOLDER'] + runfolder + "/RTAComplete.txt" )))
+nas = socket.gethostname()
+nasdir = pars['RUNFOLDER']
+rundate = list(runfolder.split("_")[0])
+rundate = "20"+rundate[0]+rundate[1]+"-"+rundate[2]+rundate[3]+"-"+rundate[4]+rundate[5]
+
+  
+nasdict = {'starttonas': starttonas, 'endtonas': endtonas, 'nas': nas, 'nasdir': nasdir, 
+           'runname': runfolder, 'startdate': rundate}
+
+res = insertorupdate( cnx, cursor, "backup", "runname", runfolder, nasdict )
+print res
+
+dbclose(cnx, cursor)
+tunnel_pid.terminate()
+exit(0)
