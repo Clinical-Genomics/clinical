b4c93bd115051caa4d7c05bea1badf16f60001ad
Rikard <rikard.erlandsson@scilifelab.se>
2015-01-16 14:33:05 +0100

    Update dbaccess.py

--- clinical/dbaccess.py
+++ clinical/dbaccess.py
@@ -44,11 +44,10 @@ def readconfig( config ):
 class create_tunnel(object):
 
     def __init__(self, tunnel_cmd):
-        print '__init__'
+#        print '__init__'
         self.tunnelcmd = tunnel_cmd
         ssh_process = subprocess.Popen(tunnel_cmd,  universal_newlines=True, shell=True, stdout=subprocess.PIPE,
                                        stderr=subprocess.STDOUT, stdin=subprocess.PIPE)
-
         while True:
           p = ssh_process.poll()
           if p is not None: break
@@ -66,11 +65,12 @@ class create_tunnel(object):
 
     def __enter__(self):
 #      print '__enter__()'
-        return self
+      return self
     
     def __exit__(self, exc_type, exc_val, exc_tb):
+      if exc_type:
         print '__exit__(%s, %s, %s)' % (exc_type, exc_val, exc_tb)
-        self.pid.terminate()
+      self.pid.terminate()
         
 #def create_tunnel(tunnel_cmd):
 #  ssh_process = subprocess.Popen(tunnel_cmd,  universal_newlines=True,
@@ -96,35 +96,49 @@ class create_tunnel(object):
 #      raise RuntimeError, 'multiple (or zero?) tunnel ssh processes found: ' + str(ssh_processes) 
 #  else:
 #    raise RuntimeError, 'Error creating tunnel: ' + str(p) + ' :: ' + str(ssh_process.stdout.readlines())
+#def dbconnect( Host, Port, Db, User, Passwd): 
+#  Cnx = mysql.connect(user=User, port=int(Port), host=Host, passwd=Passwd, db=Db, cursorclass=mysql.cursors.DictCursor)
+#  Cursor = Cnx.cursor()
+#  return Cnx, Cursor
+#def dbclose( Cnx, Cursor):
+#  Cursor.close()
+#  Cnx.close()
+#  return
 
-def dbconnect( Host, Port, Db, User, Passwd): 
-  Cnx = mysql.connect(user=User, port=int(Port), host=Host, passwd=Passwd, db=Db, cursorclass=mysql.cursors.DictCursor)
-  Cursor = Cnx.cursor()
-  return Cnx, Cursor
+class dbconnect(object):
 
-def dbclose( Cnx, Cursor):
-  Cursor.close()
-  Cnx.close()
-  return
+  def __init__(self, Host, Port, Db, User, Passwd):
+    self.cnx = mysql.connect(user=User, port=int(Port), host=Host, passwd=Passwd, db=Db, cursorclass=mysql.cursors.DictCursor)
+    self.cursor = Cnx.cursor()
 
-def generalquery( cursor, query ):
-  try:
-    cursor.execute(query)
-  except mysql.IntegrityError, e: 
-    print "Error %d: %s" % (e.args[0],e.args[1])
-    exit("DB error")
+  def __enter__(self):
+#      print '__enter__()'
+    return self
+
+  def __exit__(self, exc_type, exc_val, exc_tb):
+    if exc_type:
+      print '__exit__(%s, %s, %s)' % (exc_type, exc_val, exc_tb)
+    self.cursor.close()
+    self.cnx.close()
+
+  def generalquery( self, query ):
+    try:
+      self.cursor.execute(query)
+    except mysql.IntegrityError, e: 
+      print "Error %d: %s" % (e.args[0],e.args[1])
+      exit("DB error")
 # handle a specific error condition
-  except mysql.Error, e:
-    print "Error %d: %s" % (e.args[0],e.args[1])
-    exit("Syntax error")
+    except mysql.Error, e:
+      print "Error %d: %s" % (e.args[0],e.args[1])
+      exit("Syntax error")
 # handle a generic error condition
-  except mysql.Warning, e:
-    exit("MySQL warning")
+    except mysql.Warning, e:
+      exit("MySQL warning")
 # handle warnings, if the cursor you're using raises them
-  respons = cursor.fetchall()
-  return respons 
+    respons = self.cursor.fetchall()
+    return respons 
   
-def versioncheck( cursor, dbname, ver ):
+  def versioncheck( self, dbname, ver ):
   """Checks version of database against str( dbname) and str ( ver )  [normally from the config file]
 
   Args:
@@ -135,80 +149,80 @@ def versioncheck( cursor, dbname, ver ):
     TRUE: if identical
     str: Database name and version from table version if different
   """
-  cmd = """ SELECT major, minor, patch, name FROM version ORDER BY time DESC LIMIT 1 """
-  cursor.execute(cmd)
-  row = cursor.fetchone()
-  if row is not None:
-    major = row['major']
-    minor = row['minor']
-    patch = row['patch']
-    name = row['name']
-  else:
-    sys.exit("Incorrect DB, version not found.")
-  if (str(major)+"."+str(minor)+"."+str(patch) == ver and dbname == name):
-    return 'True'
-  else:
-    return (name + " "  + str(major) + "." + str(minor) + "." + str(patch))
+    cmd = """ SELECT major, minor, patch, name FROM version ORDER BY time DESC LIMIT 1 """
+    self.cursor.execute(cmd)
+    row = self.cursor.fetchone()
+    if row is not None:
+      major = row['major']
+      minor = row['minor']
+      patch = row['patch']
+      name = row['name']
+    else:
+      sys.exit("Incorrect DB, version not found.")
+    if (str(major)+"."+str(minor)+"."+str(patch) == ver and dbname == name):
+      return 'True'
+    else:
+      return (name + " "  + str(major) + "." + str(minor) + "." + str(patch))
   
   
-def insertorupdate( cnx, cursor, table, column, entry, insertdict ):
-  cursor.execute(""" show index from """ + table + """  """)
-  indexkey = cursor.fetchone()
-  if not indexkey:
-    return "Could not get primary key"
+  def insertorupdate( self, table, column, entry, insertdict ):
+    self.cursor.execute(""" show index from """ + table + """  """)
+    indexkey = cursor.fetchone()
+    if not indexkey:
+      return "Could not get primary key"
   
-  cursor.execute(""" SELECT """ + indexkey['Column_name'] + """ FROM """ + table + """ WHERE """ + column + """ = %s """, 
+    self.cursor.execute(""" SELECT """ + indexkey['Column_name'] + """ FROM """ + table + """ WHERE """ + column + """ = %s """, 
               (entry, ))
-  key = cursor.fetchone()
-  if key:
-    print "Entry exists ", key
-    setvalue = ""
-    for dictkey in insertdict:
-      setvalue += dictkey + "='" + insertdict[dictkey] + "', "
-    setvalue = " " + setvalue[:-2] + " "
-    print setvalue
-    print (""" UPDATE """ + table + """ SET """ + setvalue + """ WHERE """ + indexkey['Column_name'] + 
+    key = self.cursor.fetchone()
+    if key:
+      print "Entry exists ", key
+      setvalue = ""
+      for dictkey in insertdict:
+        setvalue += dictkey + "='" + insertdict[dictkey] + "', "
+      setvalue = " " + setvalue[:-2] + " "
+      print setvalue
+      print (""" UPDATE """ + table + """ SET """ + setvalue + """ WHERE """ + indexkey['Column_name'] + 
                          """ = '""" + key[indexkey['Column_name']] + """' """)
-    try:
-      cursor.execute(""" UPDATE """ + table + """ SET """ + setvalue + """ WHERE """ + indexkey['Column_name'] + 
+      try:
+        self.cursor.execute(""" UPDATE """ + table + """ SET """ + setvalue + """ WHERE """ + indexkey['Column_name'] + 
                          """ = '""" + key[indexkey['Column_name']] + """' """)
-    except mysql.IntegrityError, e: 
-      print "Error %d: %s" % (e.args[0],e.args[1])
-      exit("DB error")
+      except mysql.IntegrityError, e: 
+        print "Error %d: %s" % (e.args[0],e.args[1])
+        exit("DB error")
 # handle a specific error condition
-    except mysql.Error, e:
-      print "Error %d: %s" % (e.args[0],e.args[1])
-      exit("Syntax error")
+      except mysql.Error, e:
+        print "Error %d: %s" % (e.args[0],e.args[1])
+        exit("Syntax error")
 # handle a generic error condition
-    except mysql.Warning, e:
-      exit("MySQL warning")
+      except mysql.Warning, e:
+        exit("MySQL warning")
 # handle warnings, if the cursor you're using raises them
-    cnx.commit()
-    return True
-  else:
-    print "Entry not yet added, will be added."
-    columns = ""
-    values = ""
-    for dictkey in insertdict:
-      columns += dictkey + ", "
-      values += "'" + insertdict[dictkey] + "', "
-    columns = " (" + columns[:-2] + ") "
-    values = " (" + values[:-2] + ") " 
-    print columns, values
-    try:
-      cursor.execute(""" INSERT INTO """ + table + """  """ + columns + """ VALUES """ + values )
-    except mysql.IntegrityError, e: 
-      print "Error %d: %s" % (e.args[0],e.args[1])
-      exit("DB error")
+      self.cnx.commit()
+      return True
+    else:
+      print "Entry not yet added, will be added."
+      columns = ""
+      values = ""
+      for dictkey in insertdict:
+        columns += dictkey + ", "
+        values += "'" + insertdict[dictkey] + "', "
+      columns = " (" + columns[:-2] + ") "
+      values = " (" + values[:-2] + ") " 
+      print columns, values
+      try:
+        self.cursor.execute(""" INSERT INTO """ + table + """  """ + columns + """ VALUES """ + values )
+      except mysql.IntegrityError, e: 
+        print "Error %d: %s" % (e.args[0],e.args[1])
+        exit("DB error")
 # handle a specific error condition
-    except mysql.Error, e:
-      print "Error %d: %s" % (e.args[0],e.args[1])
-      exit("Syntax error")
+      except mysql.Error, e:
+        print "Error %d: %s" % (e.args[0],e.args[1])
+        exit("Syntax error")
 # handle a generic error condition
-    except mysql.Warning, e:
-      exit("MySQL warning")
+      except mysql.Warning, e:
+        exit("MySQL warning")
 # handle warnings, if the cursor you're using raises them
-    cnx.commit()
-    return cursor.lastrowid
+      self.cnx.commit()
+      return self.cursor.lastrowid
 
   
