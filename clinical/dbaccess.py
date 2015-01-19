ccb6ef60c0eb3840db6d785fd4ced2b26368f8ad
Rikard <rikard.erlandsson@scilifelab.se>
2015-01-19 11:27:17 +0100

    Update dbaccess.py

--- clinical/dbaccess.py
+++ clinical/dbaccess.py
@@ -89,8 +89,6 @@ class dbconnect(object):
 
   def __exit__(self, exc_type, exc_val, exc_tb):
     if exc_type:
-      if exc_type.name == '_mysql_exceptions.ProgrammingError':
-        "what"
       print '__exit__(%s, %s, %s)' % (exc_type, exc_val, exc_tb)
     self.cursor.close()
     self.cnx.close()
