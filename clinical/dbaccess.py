5a9f3a89e7b012891e703965eaee8fde3863d9b4
Rikard <rikard.erlandsson@scilifelab.se>
2015-01-19 11:26:16 +0100

    Update dbaccess.py

--- clinical/dbaccess.py
+++ clinical/dbaccess.py
@@ -144,8 +144,7 @@ class dbconnect(object):
     if not indexkey:
       return "Could not get primary key"
   
-    self.cursor.execute(""" SELECT """ + indexkey['Column_name'] + """ FROM """ + table + """ WHERE """ + column + """ = %s """, 
-              (entry, ))
+    self.cursor.execute(' SELECT {0} FROM {1} WHERE {2} \'{3}\' '.format(indexkey['Column_name'], table, column, entry, ))
     key = self.cursor.fetchone()
     if key:
       print "Entry exists ", key
