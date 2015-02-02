91214d23e5ec33de39eda8fa25b5b1f0a43c353c
Rikard <rikard.erlandsson@scilifelab.se>
2015-02-02 15:06:31 +0100

    Update samplenaming.py

--- clinical/samplenaming.py
+++ clinical/samplenaming.py
@@ -44,7 +44,7 @@ with create_tunnel(pars['TUNNELCMD']):
         if len(reply) == 1:
           for rep in reply:
 #            print rep['samplename'], name[0], name[1], str(names) #str(rep)
-          dbc.insertorupdate( 'sample', 'samplename', rep['samplename'], names )
+            dbc.insertorupdate( 'sample', 'samplename', rep['samplename'], names )
         else:
           print "NOT FOUND: ", name[0], name[1], str(names) 
  
