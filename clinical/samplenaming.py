7fab76625b37a41b594be898b55eb2fd89bd11c4
Rikard <rikard.erlandsson@scilifelab.se>
2015-02-02 09:30:15 +0100

    Update samplenaming.py

--- clinical/samplenaming.py
+++ clinical/samplenaming.py
@@ -40,8 +40,9 @@ with create_tunnel(pars['TUNNELCMD']):
         query = " SELECT sample_id, samplename, barcode FROM sample WHERE samplename LIKE '"+name[0]+"\_%' "
         print query
         reply = dbc.generalquery(query)
-        for rep in reply:
-          print name[1], str(rep)
+        if len(reply) > 1:
+          for rep in reply:
+            print name[1], str(rep)
       
     nas = socket.gethostname()
 #    nasdir = pars['RUNFOLDER']
